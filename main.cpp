03b0c3d21d5a4af2d7051c3c40f6bb39ca85b8b9
Amany-Hussein <eng.amanyhu@gmail.com>
2025-08-17 00:59:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,8 @@
-//Name: Asmaa Hussein Omar
-//ID: 20231021
-//Section num: S15
-//filters : 2 , 5 ,8 ,11 , 14
-//email : asmaahlal123456789@gmail.com
-
 //Name: Amany Hussein Mousa
 //ID: 20231026
 //Section num: S15
 //filters : 3 - 6 - 9 -12 - 13
 //email : amanyhussein2467@gmail.com
-
-//Name: Fatma Abd Elnaser Abd Elasis Marei
-//ID: 20230281
-//Section num: S15
-//filters :  1 - 4 - 7 - 10 -15
-//email : fatmaabdelnaser95@gmail.com
-
-//https://github.com/AsmaaHussein05/assignment3
 
 /*program description: Grayscale Conversion: Converts a colored image to grayscale by averaging the RGB channels.
 Black and White Conversion: Converts a colored image to black and white by setting pixel values to either black or
@@ -875,4 +861,5 @@ int main() {
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
     }
+
 }
